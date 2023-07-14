7ce98c6b8ce8f0fcc863a75897ba94991605f2f8
VenturaHaze <ijumperer@gmail.com>
2023-07-14 11:43:22 +0200

    commit from PyCharm

--- files/main.py
+++ files/main.py
@@ -47,7 +47,7 @@ def main(model_arch):
     scheduler_name = "PolynomialLRDecay"
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     BATCH_SIZE = 16
-    NUM_EPOCHS = 200
+    NUM_EPOCHS = 400
     if DEVICE == "cuda":
         NUM_WORKERS = 4
     else:
@@ -94,13 +94,13 @@ def main(model_arch):
     train_folder = 'data_train'
     train_ds = [
     # [dataset_name, fraction_of_positivies, fraction_of_negatives]
-        ['France_google', 1, 0],
-        ['France_ign', 1, 0],
-        ['Munich', 1, 0],
-        ['China', 1, 0],
-        ['Denmark', 1, 0],
-        ['Heerlen_2018_HR_output', 0, 0],
-        ['ZL_2018_HR_output', 0, 0],
+        ['France_google', 0, 0],
+        ['France_ign', 0, 0],
+        ['Munich', 0, 0],
+        ['China', 0, 0],
+        ['Denmark', 0, 0],
+        ['Heerlen_2018_HR_output', 1, 0],
+        ['ZL_2018_HR_output', 1, 0],
     ]
 
     image_dirs, mask_dirs, fractions = get_dirs_and_fractions(train_ds, parent_dir, train_folder)
